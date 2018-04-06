d35a1dbd2d15cbde7fe3ef4bcb1b25ef99ec5926
Hanmingy <bmind2001@naver.com>
2018-04-06 11:15:46 +0900

    주석 추가

--- BloodyPepper/Assets/Scripts/Object/GeneralObject.cs
+++ BloodyPepper/Assets/Scripts/Object/GeneralObject.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// 작성자 : Han
+// 작성일 : 2018-04-06
+// 클래스 명 : GeneralObject
+// 용도 : 공통 오브젝트(모든 오브젝트에 대한 공통 처리)
+
 public class GeneralObject : MonoBehaviour {
 
 	// Use this for initialization
