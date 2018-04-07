8efd1a4c9f329cc9df73735d94c76b4ffefad198
dunggamja <dunggamja@gmail.com>
2018-04-07 20:34:54 +0900

    타일 리소스 수정.

---
+++ BloodyPepper/Assets/Scripts/Tiles/GroundTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class GroundTile : Tile
+{
+    [SerializeField]
+    private Sprite[] tiles = null;
+
+    [SerializeField]
+    private Sprite preview = null;
+
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Assets/Create/Tiles/GroundTile")]
+    public static void CreateFile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("GroundTile", "NewTile", "asset", "tile");
+        if (string.IsNullOrEmpty(path))
+            return;
+
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<GroundTile>(), path);
+    }
+#endif
+}
